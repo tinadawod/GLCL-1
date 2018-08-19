6e357fc722006b259a860d56d4abd0d38a50f0cc
JUST TINA <mymac@News-MacBook-Pro.local>
2018-08-19 09:16:24 -0700

    added javascript files for the tracking page and styled the tracking page

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var list = [];
+var myFunkyFunk = function myFunkyFunk() {
+  console.log('the funk');
+};
+'use strict';
+
 var fn = function fn() {
 	var minus = document.getElementsByClassName('minus');
-	console.log(minus);
+
 	for (var i = 0; i < minus.length; i++) {
-		console.log(minus[i]);
 		var e = document.createElement('p');
 		e.innerHTML = '-';
 		minus[i].appendChild(e);
@@ -12,9 +17,3 @@ var fn = function fn() {
 };
 
 document.addEventListener('DOMContentLoaded', fn, false);
-'use strict';
-
-var list = [];
-var myFunkyFunk = function myFunkyFunk() {
-  console.log('the funk');
-};
